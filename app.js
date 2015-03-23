f9919cefc34008a77ebe2035d0cd085064c31adb
rookieoftheyear <cmaina1000@Collinss-MBP.home>
2015-03-23 10:21:24 -0400

    angular project

---
+++ app.js
@@ -0,0 +1,36 @@
+(function (){
+	var app = angular.module('store', [ ]);
+
+	app.controller('StoreController', function() {
+		this.products = gems;
+
+	});
+
+	var gems = [
+	{
+		name: "Dodecahedron",
+		price: 2.95,
+		description: ". . .",
+		images: {
+            full: 'Dodecahedron.jpeg'
+            thumb:'Dodecahedron.jpeg'
+		
+		   }
+
+		canPurchase: true,
+	},
+	{
+		name: "Pentagonal Gem",
+		price: 5.95,
+		description: ". . .",
+		images: {
+            full:'Pentagonal.jpg'
+            thumb:'Pentagonal.jpg'
+
+		   }
+
+		canPurchase: false,
+	}
+];
+
+}) () ;
